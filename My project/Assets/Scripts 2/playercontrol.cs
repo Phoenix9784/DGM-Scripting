ec43cfbc51d0cf808648bac5f88dda0ab4e954c9
Alfredo <10896965@uvu.edu>
2022-02-07 16:18:05 -0700

    Fixed the rotate

--- My project/Assets/Scripts 2/playercontrol.cs
+++ My project/Assets/Scripts 2/playercontrol.cs
@@ -23,6 +23,6 @@ public class playercontrol : MonoBehaviour
 
         //Makes the player move 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput);
-        transform.Rotate(Vector3.up * turnspeed * Time.deltaTime. * hInput);
+        transform.Rotate(Vector3.up, turnspeed * Time.deltaTime * hInput);
     }
 }
